86d3492d000f86a616356fbdc7023d510cb9c650
TigerWolf <hiddentiger@gmail.com>
2014-11-07 15:54:37 +1030

    simplify to only have LevenshteinDistance

--- call_java.rb
+++ call_java.rb
@@ -1,37 +1,7 @@
 require "java"
 
-java_import "java.util.TreeSet"
-java_import "com.example.CallMe"
-java_import "com.example.ISpeaker"
+java_import "com.example.LevenshteinDistance"
 
-puts "Hello from ruby"
-set = TreeSet.new
-set.add "foo"
-set.add "Bar"
-set.add "baz"
-set.each { |v| puts "value: #{v}" }
+cm = LevenshteinDistance.new
 
-cm = CallMe.new
-cm.hello
-
-p cm.LevenshteinDistance("hello", "world")
-# $globalCM.hello
-
-class CallJava
-	  include ISpeaker
-	    def initialize
-		        super
-			    @count = 0
-			      end
-
-	      def say(msg)
-		          puts "Ruby saying #{msg}"
-			    end
-
-	        def addOne(from)
-			#    m.synchronize {
-			          @count += 1
-			                puts "Now got #@count from #{from}"
-			                #    }
-			                  end
-			                  end
+p cm.compare("hello", "world")
